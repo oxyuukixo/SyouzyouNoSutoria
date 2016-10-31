81e9ed0de82abaaf32f37572fec8b3c2724b99d4
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-10-31 16:42:47 +0900

    テキスト作業途中

---
+++ Assets/Debu/Scripts/ConversationControll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Text;
+
+public class ConversationControll : MonoBehaviour {
+
+    //読み込むテキストのパス(シーン移動する前に設定)
+    public static string m_TextPath = "Test.txt";
+
+    private string m_TextDate;
+
+    private string[] m_DisplayText;
+
+    private FileInfo m_FileInfo;
+
+    // Use this for initialization
+    void Start () {
+        m_FileInfo = new FileInfo(Application.dataPath + "/Text/" + m_TextPath);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
