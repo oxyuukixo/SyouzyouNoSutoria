e5eea33f81868ca50e78c94c1717a9bd36e1c6fe
humiduki7 <humiduki3325@gmail.com>
2016-11-04 10:45:30 +0900

    移動範囲探索

---
+++ Assets/Yama/Scripts/test.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    public GameObject[][] m_test;
+    public int lange;
+
+	// Use this for initialization
+	void Start () {
+
+        m_test = new GameObject[5][];
+        for (int i = 0; i < 5; i++)
+        {
+            m_test[i] = new GameObject[5];
+            for (int j = 0; j < 5; j++)
+            {
+                m_test[i][j] = this.gameObject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Test();
+    }
+
+    void Test()
+    {
+        bool[][] test;
+        if (!Input.GetMouseButtonDown(0)) return;
+        test = SarchRange.PermitSarchRange( m_test, this.gameObject, lange);
+    }
+}
