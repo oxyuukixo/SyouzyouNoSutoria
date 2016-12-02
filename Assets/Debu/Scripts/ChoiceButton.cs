395a6b2f03a68698af6c90d7e7bf32dd38f03894
oxyuukixo <tya_mtd@cpost.plala.or.jp>
2016-12-02 15:40:45 +0900

    テキスト新完成

---
+++ Assets/Debu/Scripts/ChoiceButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoiceButton : MonoBehaviour {
+
+    //ボタンが押されたかのフラグ
+    [HideInInspector]
+    public bool m_IsPush = false;
+
+    //ボタンが押された時に読み込むテキスト
+    [HideInInspector]
+    public string m_ReadText;
+
+    public void ButtonPush()
+    {
+        m_IsPush = true;
+    }
+}
