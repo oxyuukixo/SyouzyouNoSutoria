49987023530fb94683faf84d0910603c6ae11161
keisuke1022 <zkms_keikei@outlook.jp>
2016-12-20 18:17:58 +0900

    タイトル、オプション

---
+++ Assets/Kei/script/NewGameButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameButton: MonoBehaviour {
+
+
+    public void ButtonPush()
+    {
+        FadeManager.Instance.LoadLevel("Conversation", 1.0f);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
